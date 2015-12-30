5562952f9de95eb1c7456f2a62ab4695a90098fb
Thibault Réal <thije81@hotmail.com>
2015-12-30 02:51:20 +0100

    modif changement test

--- testGithubWamp.php
+++ testGithubWamp.php
@@ -4,11 +4,14 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title>test Github and wamp</title>
     <b>test github wamp</b>
+    <b>test détection modification github</b>
+    <!-- dfsgsqlidfhq-->
     </head>
     <body>
         <?php
